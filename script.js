212565bbc87c64f9cc657e27ab6f0a082c665e36
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-20 12:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+  let totalPrice = 0;
+
+  prices.forEach(price => {
+    totalPrice += parseInt(price.innerText);
+  });
+
+  const newRow = document.createElement("tr");
+  const totalCell = document.createElement("td");
+  totalCell.innerText = "Total Price:";
+  const priceCell = document.createElement("td");
+  priceCell.innerText = totalPrice;
+  newRow.appendChild(totalCell);
+  newRow.appendChild(priceCell);
+  document.querySelector("table").appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
